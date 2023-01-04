4fbbaebd362acd71a566e63eec476c4b4a54605a
Ildar Sharafeev <i_sharafeev@apple.com>
2023-01-03 21:32:01 -0800

    Add gitignore

--- .aws-sam/auto-dependency-layer/CreateOrUpdateToDo/apiClients/ddbClient.js
+++
@@ -1,9 +0,0 @@
-const DynamoDb = require("aws-sdk/clients/dynamodb");
-
-const ddbClient = new DynamoDb({
-  ...(process.env.AWS_SAM_LOCAL === "true"
-    ? { endpoint: process.env.DDB_ENDPOINT_URL }
-    : {}),
-});
-
-module.exports = ddbClient;
